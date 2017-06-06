c12caa1f7050581e03e6e7d6aa491c26bbb2fffb
Martin Lidy <Martin Lidy>
2017-06-06 12:42:54 -0600

    Add custom css so page width will not be limited

--- src/conf.py
+++ src/conf.py
@@ -90,6 +90,8 @@ todo_include_todos = True
 #
 html_theme = 'sphinx_rtd_theme'
 
+html_style = 'css/my_theme.css'
+
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
